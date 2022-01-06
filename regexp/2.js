9dfbe6a89a7ffa4ce4af3290a2079588215522ed
wudan01 <wudan01@100tal.com>
2022-01-06 09:18:27 +0800

    add

--- regexp/2.js
+++ regexp/2.js
@@ -3,6 +3,8 @@
  * @description: 千分符设置
  */
 
+// 方法1 
+/*
 var str = '12344565645';
 str = str.split('').reverse().join('');
 
@@ -14,3 +16,18 @@ str = str.replace(/\d{3}/g, function ($0, $1) {
 
 str = str.split('').reverse().join('');
 console.log(str);
+*/
+
+// 方法2
+
+var str = '12344565645';
+
+// (?= (\d{ 3 }) +$) 表示匹配 3个一组结尾的， ?= 表示会从尾向前匹配
+
+str = str.replace(/\d{1,3}(?=(\d{3})+$)/g, function ($0) {
+  return $0 + ',';
+});
+
+console.log(str)
+
+
