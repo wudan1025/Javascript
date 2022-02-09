b5786d5626959898125f2ada4aa23b0bc61a294c
wudan <xes@wudandeMacBook-Pro.local>
2022-02-09 18:44:38 +0800

    add

---
+++ js/jquery/1.js
@@ -0,0 +1,7 @@
+/*
+ * @LastEditors: wudan01
+ * @description: 文件描述
+ */
+// 20201222 - 20201227
+
+// jquery 源码
---
+++ js/同步异步/1.js
@@ -0,0 +1,6 @@
+/*
+ * @LastEditors: wudan01
+ * @description: 文件描述
+ */
+// 20201230 - 20210103
+// 自己实现promise
