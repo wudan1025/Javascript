e143c3a9d902b5cb832909708bec09d1011147dd
wudan <xes@wudandeMacBook-Pro.local>
2022-02-17 18:20:31 +0800

    add

--- js/2. scope/6.js
+++ js/2. scope/6.js
@@ -11,41 +11,82 @@ var submit = document.getElementById('load');
 
 submit.addEventListener(
   'click',
-  throttle(3000, function () {
-    console.log('click2');
-  })
+  throttle(
+    function () {
+      console.log('click2');
+    },
+    3000,
+    true
+  )
 );
 
-// todo 写完
-function throttle(time, cb) {
+// 第一次点击 remain s 后执行，这之间的点击不响应
+function throttle_my(remain, cb, immediate) {
   // 此时不能 按照上一次时间记录，因为这是外层代码执行时间
   // 不是内层函数执行时间
-  var pre = new Date().getTime(),
-    now;
+  var pre, now;
   var exec = false;
   var timer = null;
 
   return function () {
-    // if (!exec) {
-    //   cb();
-    //   exec = true;
-    // }
+    if (immediate && !exec) {
+      cb();
+      exec = true;
+    }
     if (timer) {
       now = new Date().getTime();
     } else {
+      // 第一次进入
       pre = now = new Date().getTime();
-      timer = setTimeout(() => {
-        cb();
-      }, time);
-    }
 
-    console.log(now - pre);
-    if (now - pre > time) {
+      timer = setTimeout(() => {
+        if (!immediate) {
+          cb();
+        }
+      }, remain);
+    }
+    // console.log(now - pre);
+    if (now - pre > remain) {
       clearTimeout(timer);
       timer = null;
       pre = now;
       exec = false;
-      // cb();
     }
+  };
+}
+
+function throttle(func, wait) {
+  if (typeof func !== 'function')
+    throw new TypeError('func must be a function!');
+  wait = +wait;
+  if (isNaN(wait)) wait = 300;
+  var timer = null,
+    previous = 0,
+    result;
+  return function proxy() {
+    var self = this,
+      params = [].slice.call(arguments);
+    var now = +new Date(),
+      remaining = wait - (now - previous);
+    if (remaining <= 0) {
+      // 两次间隔时间已经超过WAIT了，此时我们立即执行
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      previous = now;
+      result = func.apply(self, params);
+      return result;
+    }
+    // 没有定时器我们才设置，有定时器说明上次还没执行呢，只有上一次执行了，我们在开启下一次
+    if (!timer) {
+      timer = setTimeout(function () {
+        clearTimeout(timer);
+        timer = null;
+        previous = +new Date();
+        result = func.apply(self, params);
+      }, remaining);
+    }
+    return result;
   };
 }
