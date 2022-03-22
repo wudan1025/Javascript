e538d8aa6e6ee622124a471141dd4d642d128f0b
wudan <xes@wudandeMacBook-Pro.local>
2022-03-22 18:30:52 +0800

    ad

---
+++ ts/test/类型/1.ts
@@ -0,0 +1,12 @@
+/*
+ * @LastEditors: wudan01
+ * @description: 文件描述
+ */
+let y: number;
+
+// null undefind config 设置?
+y = null; // 无法通过编译
+y = undefined; // 无法通过编译
+
+// y = null!;
+// y = undefined!;
